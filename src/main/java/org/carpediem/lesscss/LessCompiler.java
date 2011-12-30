a47a5fbe7f256e465e69a8070411923b26548e8f
Mark S. Petrovic <mspetrovic@gmail.com>
2011-12-30 10:56:59 -0800

    Configure Maven surefire plugin to not use files.  Output test results to console.

--- src/main/java/org/carpediem/lesscss/LessCompiler.java
+++ src/main/java/org/carpediem/lesscss/LessCompiler.java
@@ -21,46 +21,27 @@ import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.ScriptableObject;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class LessCompiler {
+final public class LessCompiler {
+    private final Context context;
+    private final ScriptableObject scriptableObject;
+    private final String LESS_SRC = "org/carpediem/lesscss/less-1.6.1.js";
+    private final String RUN_SRC = "org/carpediem/lesscss/run.js";
 
-    private Context context;
-    private ScriptableObject scriptableObject;
-
-    public static void main(String[] args) throws IOException {
-        try {
-            LessCompiler lessCompiler = new LessCompiler();
-            String t = lessCompiler.compile(new FileInputStream("style.less"));
-            System.out.print("result: " + t);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public LessCompiler() {
-        String lessSrc = "org/carpediem/lesscss/less-1.6.1.js";
-        String runSrc = "org/carpediem/lesscss/run.js";
-
-        final InputStream lessIs = getClass().getClassLoader().getResourceAsStream(lessSrc);
-        final InputStream runIs = getClass().getClassLoader().getResourceAsStream(runSrc);
-        try {
-            final ContextFactory contextFactory = new ContextFactory();
-            context = contextFactory.enterContext();
-            scriptableObject = context.initStandardObjects();
-            context.setOptimizationLevel(9);
-
-            context.evaluateString(scriptableObject, asString(lessIs), lessSrc, 1, null);
-            context.evaluateString(scriptableObject, asString(runIs), runSrc, 1, null);
-            lessIs.close();
-            runIs.close();
-        } catch (IOException ex) {
-            throw new IllegalStateException("Failed reading javascript less.js", ex);
-        }
+    public LessCompiler() throws IOException {
+        final InputStream lessIs = getClass().getClassLoader().getResourceAsStream(LESS_SRC);
+        final InputStream runIs = getClass().getClassLoader().getResourceAsStream(RUN_SRC);
+        final ContextFactory contextFactory = new ContextFactory();
+        context = contextFactory.enterContext();
+        scriptableObject = context.initStandardObjects();
+        context.setOptimizationLevel(9);
+        context.evaluateString(scriptableObject, asString(lessIs), LESS_SRC, 1, null);
+        context.evaluateString(scriptableObject, asString(runIs), RUN_SRC, 1, null);
+        lessIs.close();
+        runIs.close();
     }
 
     public String compile(InputStream input) throws IOException {
@@ -68,18 +49,16 @@ public class LessCompiler {
         final String replace = data.replace("\"", "\\\"").replaceAll("\n", "").replaceAll("\r", "");
         StringBuilder sb = new StringBuilder("lessIt(\"").append(replace).append("\")");
         String lessitjs = sb.toString();
-        String result = context.evaluateString(scriptableObject, lessitjs, "lessitjs.js", 1, null).toString();
-        return result;
+        return context.evaluateString(scriptableObject, lessitjs, "lessitjs.js", 1, null).toString();
     }
 
     private String asString(InputStream inputStream) throws IOException {
-        String line;
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
         while ((line = br.readLine()) != null) {
             sb.append(line);
         }
-        String s = sb.toString();
-        return s;
+        return sb.toString();
     }
 }
--- src/test/java/org/carpediem/lesscss/LessCompilerTest.java
+++ src/test/java/org/carpediem/lesscss/LessCompilerTest.java
@@ -1,18 +1,18 @@
 package org.carpediem.lesscss;
 
-import org.carpediem.lesscss.LessCompiler;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class LessCompilerTest {
+    private final String UTF_8 = "UTF-8";
 
     @Test
     public void testApp() throws IOException {
         String input = "@color: #4D926F; #header { color: @color; } h2 { color: @color; }";
         LessCompiler lessCompiler = new LessCompiler();
-        final String compile = lessCompiler.compile(new ByteArrayInputStream(input.getBytes("UTF-8")));
+        final String compile = lessCompiler.compile(new ByteArrayInputStream(input.getBytes(UTF_8)));
         System.out.println(compile);
     }
 }
