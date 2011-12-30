8c91bd205747fb690e4cb58f8c723d378e3cafd1
Mark S. Petrovic <mspetrovic@gmail.com>
2011-12-30 13:01:51 -0800

    Use less version 1.1.5, which is the most recent version on the lesscss.org site.

--- src/main/java/org/carpediem/lesscss/LessCompiler.java
+++ src/main/java/org/carpediem/lesscss/LessCompiler.java
@@ -25,7 +25,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 final public class LessCompiler {
-    private final String LESS_SRC = "org/carpediem/lesscss/less-1.6.1.js";
+    private final String LESS_SRC = "org/carpediem/lesscss/less-1.1.5.min.js";
     private final String RUN_SRC = "org/carpediem/lesscss/run.js";
     private final ScriptEngine scriptEngine;
 
