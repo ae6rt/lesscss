a7d0c1aa09f0272daf9e6bd7695cdc91a8ca9243
Mark S. Petrovic <mspetrovic@gmail.com>
2011-12-31 17:14:56 -0800

    Try to add engine.js and env.js to script engine to fix undefined js variable problem

--- src/main/java/org/carpediem/lesscss/LessCompiler.java
+++ src/main/java/org/carpediem/lesscss/LessCompiler.java
@@ -27,17 +27,23 @@ import java.io.InputStreamReader;
 final public class LessCompiler {
     private final String LESS_SRC = "org/carpediem/lesscss/less-1.1.5.js";
     private final String RUN_SRC = "org/carpediem/lesscss/run.js";
+    private final String ENGINE_SRC = "org/carpediem/lesscss/engine.js";
+    private final String ENV_SRC = "org/carpediem/lesscss/env.js";
     private final ScriptEngine scriptEngine;
 
     public LessCompiler() throws IOException, ScriptException {
-        final InputStream lessIs = getClass().getClassLoader().getResourceAsStream(LESS_SRC);
-        final InputStream runIs = getClass().getClassLoader().getResourceAsStream(RUN_SRC);
+        final ClassLoader classLoader = getClass().getClassLoader();
         final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
         scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
-        scriptEngine.eval(asString(lessIs));
-        scriptEngine.eval(asString(runIs));
-        lessIs.close();
-        runIs.close();
+        for (String s : new String[]{ENV_SRC, LESS_SRC, ENGINE_SRC, RUN_SRC}) {
+            final InputStream resourceAsStream = classLoader.getResourceAsStream(s);
+            scriptEngine.eval(new InputStreamReader(resourceAsStream));
+            resourceAsStream.close();
+            if (s.equals(ENGINE_SRC)) {
+                scriptEngine.eval("lessenv.charset = '" + "UTF-8" + "';");
+                scriptEngine.eval("lessenv.css = " + "false" + ";");
+            }
+        }
     }
 
     public String compile(InputStream input) throws IOException, ScriptException {
